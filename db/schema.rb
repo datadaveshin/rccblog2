01ac0da4f595ebd737e443cf188c04ec073cb0bf
David Shin <davidshin444@gmail.com>
2019-01-22 20:23:24 -0800

    Generate a new column in posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_005247) do
+ActiveRecord::Schema.define(version: 2019_01_23_041600) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
