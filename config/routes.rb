f3bf31846d0f25b2b5a236d6b7f6f1f9a45c9f50
David Shin <davidshin444@gmail.com>
2019-02-05 18:19:29 -0800

    47: Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :commments, only: :create
+  end
+
+  root 'posts#index'
 end
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
