ca2b1fdc7cf7c0464aa134faafef5b02e3904fe5
David Shin <davidshin444@gmail.com>
2019-02-11 20:02:07 -0800

    Minor tweaks

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
 
-    if @post.comments.create(comments_params)
+    if comments_params[:email] == "address" && @post.comments.create(comments_params)
       redirect_to @post, notice: 'Comment was successfully created.'
     else
       redirect_to @post, alert: "the following errors occurred #{@post.errors.inspect}"
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     respond_to do |format|
-      if @post.save
+      if @post.email == "address" && @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :email)
     end
 end
